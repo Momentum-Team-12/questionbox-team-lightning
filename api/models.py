caa28694b5022f341feac0b38513e01c026e12fc
Shawna Cooper <scooper1920@gmail.com>
2022-06-01 17:19:08 -0400

    (models created)

--- api/models.py
+++ api/models.py
@@ -7,3 +7,35 @@ class User(AbstractUser):
    
     def __str__(self):
         return self.username
+
+class Question(models.Model):
+    name        = models.CharField(max_length=300, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    creator     = models.ForeignKey('User', related_name='questions', on_delete=models.CASCADE, null=True,blank=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.name
+
+
+class Answer(models.Model):
+    response   = models.TextField(null=True, blank=True)
+    responder  = models.ForeignKey('User', related_name='answers', on_delete=models.CASCADE, null=True,blank=True)
+    question   = models.ForeignKey('Question', related_name='answers', on_delete=models.CASCADE, null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Accepted(models.Model):
+    response = models.ForeignKey('Answer', related_name='accepteds',on_delete=models.CASCADE)
+    #foreign key = user.pk
+    user = models.ForeignKey('User', related_name= 'accepteds', on_delete=models.CASCADE)
+
+
+
+
+
+class Favorite(models.Model):
+    question = models.ForeignKey('Question', related_name='favorites',on_delete=models.CASCADE)
+    #foreign key = user.pk
+    user = models.ForeignKey('User', related_name= 'favorites', on_delete=models.CASCADE)
