e592aed1c31f44629fabacbd2d6ecb85c0f833b5
Shawna Cooper <scooper1920@gmail.com>
2022-06-02 19:14:15 -0400

    (urls)

--- questionbox/urls.py
+++ questionbox/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api/', include(router.urls))
 
