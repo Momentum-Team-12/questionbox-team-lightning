ec1c4e0e36a05e758994de4fe54d288a1717d521
Shawna Cooper <scooper1920@gmail.com>
2022-06-02 10:05:02 -0400

    (favorites )

--- api/models.py
+++ api/models.py
@@ -13,6 +13,7 @@ class Question(models.Model):
     description = models.TextField(null=True, blank=True)
     creator     = models.ForeignKey('User', related_name='questions', on_delete=models.CASCADE, null=True,blank=True)
     created_at  = models.DateTimeField(auto_now_add=True)
+    favorite    = models.BooleanField(default=False)
 
     def __str__ (self):
         return self.name
@@ -23,7 +24,7 @@ class Answer(models.Model):
     responder  = models.ForeignKey('User', related_name='answers', on_delete=models.CASCADE, null=True,blank=True)
     question   = models.ForeignKey('Question', related_name='answers', on_delete=models.CASCADE, null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    favorite   = models.BooleanField(default=False)
+    
 
 
 
