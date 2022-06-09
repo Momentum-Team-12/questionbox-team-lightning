abbb47077e7cf751741958a82d979365ba9888e1
Shawna Cooper <scooper1920@gmail.com>
2022-06-09 18:28:40 -0400

    (permissions)

--- api/permissions.py
+++ api/permissions.py
@@ -43,6 +43,6 @@ class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if obj.user is request.user:
+        if obj.user == request.user:
             return True
         return False
